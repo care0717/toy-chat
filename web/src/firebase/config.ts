e045266e27bd494612f62d6553640974f57056c6
asai <dev.care0717@gmail.com>
2020-05-07 13:57:23 +0900

    feat: copy message syori

--- web/src/firebase/config.ts
+++ web/src/firebase/config.ts
@@ -1,10 +1,15 @@
-export const firebaseConfig = {
-    apiKey: "AIzaSyA_zS_o97uYk5lo6nbyfQSsePUc8dYryTA",
-    authDomain: "toychat-a5115.firebaseapp.com",
-    databaseURL: "https://toychat-a5115.firebaseio.com",
-    projectId: "toychat-a5115",
-    storageBucket: "toychat-a5115.appspot.com",
-    messagingSenderId: "675083918152",
-    appId: "1:675083918152:web:ce2d395272af251c03317d",
-    measurementId: "G-2R6G6Q2CZ4"
+import firebase from "firebase";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA_zS_o97uYk5lo6nbyfQSsePUc8dYryTA",
+  authDomain: "toychat-a5115.firebaseapp.com",
+  databaseURL: "https://toychat-a5115.firebaseio.com",
+  projectId: "toychat-a5115",
+  storageBucket: "toychat-a5115.appspot.com",
+  messagingSenderId: "675083918152",
+  appId: "1:675083918152:web:ce2d395272af251c03317d",
+  measurementId: "G-2R6G6Q2CZ4",
 };
+
+export const firebaseApp = firebase.initializeApp(firebaseConfig);
+export const firebaseDb = firebaseApp.database();
